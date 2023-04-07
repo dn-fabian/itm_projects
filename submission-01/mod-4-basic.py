1b350d591ad3472fe3d054c86c23c132c36f9ce3
Dawn Fabian <dawn.fabian@obf.ateneo.edu>
2023-04-07 17:07:31 +0800

    final file

--- submission-01/mod-4-basic.py
+++ submission-01/mod-4-basic.py
@@ -10,7 +10,7 @@ def savings(gross_pay, tax_rate, expenses):
     tax_amount = tax_rate * gross_pay
     after_tax = math.floor(gross_pay - tax_amount)
     take_home_pay = int(after_tax) - expenses
-    print(take_home_pay)
+    # print(take_home_pay)
     return take_home_pay
 
 # savings(50_000, 0.10, 25_000)
@@ -19,7 +19,7 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
     total_mat_consumed = num_jobs * job_consumption
     wasted_material = total_material - total_mat_consumed
     remaining_material = str(wasted_material) + material_units
-    print(remaining_material)
+    # print(remaining_material)
     return remaining_material
 
 # material_waste(1_000, "kg", 10, 5)
@@ -27,7 +27,7 @@ def material_waste(total_material, material_units, num_jobs, job_consumption):
 def interest(principal, rate, periods):
     simple_interest = principal * rate * periods
     final_value = math.floor(principal + int(simple_interest))
-    print(final_value)
+    # print(final_value)
     return final_value
 
 # interest(2_000, 0.05, 5)
@@ -38,8 +38,9 @@ def body_mass_index(weight, height):
     height_in = height[1]*0.0254
     total_height = height_ft + height_in
     bmi = weight_in_kg / (total_height**2)
-    print(bmi)
+    # print(bmi)
     return(bmi)
+
 # body_mass_index(187.393, [5, 8])
 
 
